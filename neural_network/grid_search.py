a5e0ce3fce135e4db59cc56fab3f89cd8e736ec3
Remo <andreoliremo@gmail.com>
2019-01-20 03:29:58 +0100

    added RandomSearch

--- neural_network/grid_search.py
+++ neural_network/grid_search.py
@@ -6,10 +6,11 @@ from lr_schedulers import ExponentialDecayScheduler, TimeBasedScheduler, StepDec
 import numpy as np
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import ParameterGrid
+import random
 
 class GridSearch:
     """
-    GridSearch implemenation
+    GridSearch implementation
         Attributes:
         tuning_params(dict) - parameters to tune
         estimator(neural_network) - the neural network to fit with the tuned parameters
@@ -29,11 +30,48 @@ class GridSearch:
 
         for params in grid:
             self.estimator.compile(optimizer=SGD(lr_init=params['lr'], momentum=params['momentum'], nesterov=params['nesterov'], lr_sched=StepDecayScheduler(epochs_drop=15)))
-            self.estimator.fit(dataset, targets, batch_size=params['batch_size'], test_size=params['test_size'], epochs=params['epoch'], patience=params['patience'], save_stats=True) #lr=0.05 test_size=0.5 epochs=2000
+            self.estimator.fit(dataset, targets, batch_size=params['batch_size'], test_size=params['test_size'], epochs=params['epoch'], patience=params['patience'], save_stats=True)
 
             accuracy=np.amax(np.load("vl_accuracy.npy"))
             if(accuracy > best_accuracy):
                 best_accuracy=accuracy
                 best_param={x:params[x] for x in list(params.keys())}
                 
+        return best_accuracy, best_param
+
+class RandomSearch:
+    """
+    RandomSearch implementation
+        Attributes:
+        tuning_params(dict) - parameters to tune
+        estimator(neural_network) - the neural network to fit with the tuned parameters
+        iterations(int) - how many iteration
+    """
+    def __init__(self, tuning_params, estimator, iterations):
+        self.tuning_params=tuning_params
+        self.estimator=estimator
+        self.iterations=iterations
+    
+    def fit(self, dataset, targets):
+        """
+        Fit the estimator with the given dataset and targets picking random values
+        :param dataset: data on which the model will fit
+        :param targets: ground_truth values of the given data
+        """
+        best_accuracy=0
+
+        for i in range(self.iterations):
+            params={}
+            for key in self.tuning_params:
+                params[key]=random.choice(self.tuning_params[key])
+
+            self.estimator.compile(optimizer=SGD(lr_init=params['lr'], momentum=params['momentum'], nesterov=params['nesterov'], lr_sched=StepDecayScheduler(epochs_drop=15)))
+            self.estimator.fit(dataset, targets, batch_size=params['batch_size'], test_size=params['test_size'], epochs=params['epoch'], patience=params['patience'], save_stats=True)
+
+            
+            accuracy=np.amax(np.load("vl_accuracy.npy"))
+            if(accuracy > best_accuracy):
+                best_accuracy=accuracy
+                best_param={x:params[x] for x in list(params.keys())}
+        
         return best_accuracy, best_param
